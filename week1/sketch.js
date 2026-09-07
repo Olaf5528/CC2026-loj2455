b8bb900c54fd994850c8c80af362380e1dfa8f92
Olaf5528 <loj2455@nyu.edu>
2026-09-07 10:29:45 -0400

    NetID

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,7 +2,8 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
+  { text: "Creative coding is a process of exploring, experimenting, reflecting, and discovering, 
+    using code as the main tool to create different forms of media and art.", source: "Tim Rodenbröker" },
   { text: "a chance for code to be wrong", source: "Sara" },
   { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
   { text: "process over results", source: "Sam" },
