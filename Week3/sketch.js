422362ccc81fa59b389b8505d8df852dd846f055
Olaf5528 <loj2455@nyu.edu>
2026-09-16 16:59:08 -0400

    Fixed

---
+++ Week3/sketch.js
@@ -0,0 +1,63 @@
+let x
+
+
+
+
+/*
+
+//LOOPS
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+    background(200);
+    // while loops
+    //for(thing to check, what to check against, what to do when you are done with a loop)
+    noFill();
+    for(let i = 0; i<=8; i++){
+        //do the loo[ here
+        ellipse(i*100 + 50, 100, i*10+10);
+        //console.log(i);
+    }
+    noLoop();
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let sqSize, sqX, sqX,
+let xDir = 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  sqX = with/2;
+  sqY = height/2;
+  sqSize = 100;
+  rectMode(CENTER);
+  strokeWeight(2);
+}
+
+function draw() {
+  background(220);
+  if (mouseX < width/2){
+    fill(0);
+    console.log('mouse is on the left')
+  } else {
+    fill(255);
+  }
+}
+*/
